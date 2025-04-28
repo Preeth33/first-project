d6b70aa36954bb1e0debd0b676235f7c2921fdb7
preeth <preethvijaypalaniappan@gmail.com>
2025-04-28 12:18:59 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+// File: /C:/Users/preet/first-project/index.java
+
+package firstproject;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("The value of c is: " + c);
+    }
+}
