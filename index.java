76b16a5372090925c8cebe2b38daf775d02843c8
Your Name <your_email@example.com>
2025-04-28 11:27:05 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,12 @@
+// File: /C:/Users/preet/first-project/index.java
+
+package firstproject;
+
+public class Main {
+    public static void main(String[] args) {
+    a=1;
+    b=2
+    c=a+b;
+        System.out.println("c");
+    }
+}
